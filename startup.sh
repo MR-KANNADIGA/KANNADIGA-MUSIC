282a1e4aa814d677ec0aa7b8e667a5eea5df71bf
ᴹᴿ KANNADIGA <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-03-09 10:27:59 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/LEGEND-AI/LEGEND-MUSIC /LEGEND-MUSIC
+git clone https://github.com/MR-KANNADIGA/KANNADIGA-MUSIC /KANNADIGA-MUSIC
 echo ">> INSTALLING REQUIREMENTS..."
-cd /LEGEND-MUSIC
+cd /KANNAFIGA-MUSIC
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
